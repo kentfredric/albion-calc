b4cac902b1480c1e2cfa212a9adb639ea52c1617
Kent Fredric <kentfredric@gmail.com>
2017-07-14 05:43:39 +1200

    Add albionitem tests

---
+++ tests/AlbionItem.js
@@ -0,0 +1,66 @@
+window.add_suite(function(){
+  subtest("AlbionItem", function() {
+    subtest("No Reso/Attrs", function() {
+
+      let item = new AlbionItem( "stone" );
+ 
+      is( item.has_tier() , false , "No tier specified" );
+      is( item.tier()     , 1     , "Fallback teir is 1");
+      is( item.is_tier(1) , false , "Items without tier cant be == a tier" );
+      is( item.has_attribute("invalid"), false    , "Invalid attributes don't exist");
+      is( item.get_attribute("invalid"), undefined, "Invalid attributes return undef");
+
+      ok( Array.isArray( item.crafting_resource_names()), "crafting resource names is an array");
+      is( item.crafting_resource_names().length , 0     , "Array is empty" );
+      is( item.crafting_requires("invalid")     , false , "Invalid resources can't be required" );
+      is( item.get_crafting_cost("invalid")     , 0     , "Invalid resources have 0 crafting cost" );
+
+      ok( Array.isArray( item.transmuting_resource_names()), "transmuting resource names is an array");
+      is( item.transmuting_resource_names().length, 0     , "Array is empty" );
+      is( item.transmuting_requires("invalid")    , false , "Invalid resources can't be required" );
+      is( item.get_transmuting_cost("invalid")    , 0     , "Invalid resources have 0 transmuting cost" );
+      is( item.transmutable()                     , false , "Not a transmutable item" );
+      ok( Array.isArray( item.all_resource_names()),        "all resource names is an array"  );
+      is( item.all_resource_names().length         , 0     , "Array is empty"                 );
+      is( item.get_cost("invalid")                 , 0     , "Invalid resources have 0 cost"  );
+    });
+
+    subtest("Basic", function() {
+
+      let item = new AlbionItem( "stone", { "pebbles" : 5 } );
+ 
+      is( item.has_tier() , false , "No tier specified" );
+      is( item.tier()     , 1     , "Fallback teir is 1");
+      is( item.is_tier(1) , false , "Items without tier cant be == a tier" );
+      is( item.has_attribute("invalid"), false    , "Invalid attributes don't exist");
+      is( item.get_attribute("invalid"), undefined, "Invalid attributes return undef");
+
+      ok( Array.isArray( item.crafting_resource_names()), "crafting resource names is an array");
+      is( item.crafting_resource_names().length , 1     , "Array has 1 item" );
+      is( item.crafting_requires("pebbles")     , true  , "Resource requires pebbles" );
+      is( item.get_crafting_cost("pebbles")     , 5     , "Resource requires 5 pebbles each" );
+
+      ok( Array.isArray( item.transmuting_resource_names()), "transmuting resource names is an array");
+      is( item.transmuting_resource_names().length, 0     , "Array is empty" );
+      is( item.transmuting_requires("invalid")    , false , "Invalid resources can't be required" );
+      is( item.get_transmuting_cost("invalid")    , 0     , "Invalid resources have 0 transmuting cost" );
+      is( item.transmutable()                     , false , "Not a transmutable item" );
+      ok( Array.isArray( item.all_resource_names()),        "all resource names is an array"  );
+      is( item.all_resource_names().length         , 1     , "Array has 1 item"               );
+      is( item.get_cost("pebbles")                 , 5     , "Pebbles exposed to generic cost API"  );
+    });
+
+    subtest("Tiers/attributes", function() {
+      let item = new AlbionItem( "stone", { }, { tier: 4 } );
+      is( item.has_tier() , true  , "tier specified" );
+      is( item.tier()     , 4     , "teir is 4");
+      is( item.is_tier(1) , false , "tier 4 != teir 1" );
+      is( item.is_tier(4) , true  , "tier 4 == teir 4" );
+      is( item.has_attribute("tier"), true    , "teir attribute exists");
+      is( item.get_attribute("tier"), 4       , "teir attribute has expected value");
+
+    });
+
+  });
+  done_testing();
+});
---
+++ tests/driver.js
@@ -0,0 +1,105 @@
+(function() {
+  var output;
+
+  
+  window.note = function(msg) {
+    let node = document.createElement("li");
+    node.textContent = msg;
+    node.className   = "note";
+    output.appendChild(node);
+    return node;
+  };
+  window.pass = function(msg) {
+    let node = document.createElement("li");
+    node.textContent = msg;
+    node.className = "pass";
+    output.appendChild(node);
+    return node;
+  };
+  window.fail = function(msg) {
+    let node = document.createElement("li");
+    node.textContent = msg;
+    node.className = "fail";
+    output.failed = true;
+    output.appendChild(node);
+    return node;
+  };
+  window.ok = function( state, msg ) {
+    if ( state ) {
+      return window.pass(msg);
+    }
+    window.fail(msg);
+  };
+  window.is = function( got, ex, msg ) {
+    if ( got == ex ) {
+      return window.pass(msg);
+    }
+    let diag = document.createElement("ul");
+    diag.className = "diag";
+    let got_content = document.createElement("li");
+    got_content.textContent = "got:" + got;
+    got_content.className = "got";
+    diag.appendChild(got_content);
+    let exp_content = document.createElement("li");
+    exp_content.textContent = "expected:" + ex;
+    exp_content.className = "expected";
+    diag.appendChild(exp_content);
+    let fail = window.fail(msg);
+    fail.appendChild( diag );
+    return fail;
+  };
+  window.subtest = function( reason, callback ) {
+    /* set up dom */
+    var subtest_node = document.createElement("li");
+    subtest_node.className = "subtest";
+    subtest_node.textContent = reason;
+    var new_output = document.createElement("ol");
+    subtest_node.appendChild( new_output );
+    /* Inject target now so failed tests will show progress */
+    output.appendChild(subtest_node);
+    /* Swap vars that tests call */
+    var old_output = output;
+    output = new_output;
+    callback();
+    if ( output.hasOwnProperty('failed') ) {
+      old_output.failed = true;
+      subtest_node.classList.add('fail');
+      new_output.classList.add('fail');
+    } else {
+      subtest_node.classList.add('pass');
+      new_output.classList.add('pass');
+
+    }
+    output = old_output;
+    return subtest_node;
+  };
+  window.done_testing = function() {
+    if ( output.hasOwnProperty('failed') ) {
+      output.classList.add("fail");
+    } else {
+      output.classList.add("pass");
+    }
+  };
+  let test_suites = [];
+  let loaded = false;
+  window.onload = function() {
+    loaded = true;
+    output = document.getElementById("output");
+    let test_seq = 1;
+    output.get_test_id = function() {
+      let my_seq = test_seq;
+      test_seq++;
+      return my_seq;
+    };
+    for ( let test of test_suites ) {
+      test();
+    }
+  };
+  window.add_suite = function( callback ) {
+    if ( loaded ) {
+      callback();
+    } else {
+      test_suites.push(callback);
+    }
+  };
+})();
